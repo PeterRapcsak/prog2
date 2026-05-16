1e36177bc2c7047b80b87ec935e1071910957997
PeterRapcsak <peter.rapcsak@gmail.com>
2026-05-16 20:29:59 +0200

    xds

--- lab10/PString-master/pstring.cpp
+++ lab10/PString-master/pstring.cpp
@@ -1,12 +1,54 @@
-/**
- * \file pstring.cpp
- *
- * Szerializálható String osztály (PString = Persistent String)
- * implementációja.
- *
- * A PString osztály az összes tagfüggvényét az header-ben valósítja meg,
- * mivel az String és Serializable interfészből örökli a tagfüggvényeket.
- *
- */
-
 #include "pstring.h"
+#include <iostream>
+#include <cstddef>
+
+
+//! ---------- KONSTRUKTOROK ----------
+
+PString::PString(const char *p) : String(p) {}
+
+PString::PString(char ch) : String(ch) {}
+
+PString::PString(const String& s) : String(s) {}
+
+PString::PString(const PString& ps) : String(ps) {}
+
+
+//! ---------- KIÍRÁS ----------
+
+// CÉL: A sztring bináris mentése
+void PString::write(std::ostream& os) const {
+    std::size_t len = this->size(); // String mérete
+
+    // Hossz kiírása binárisan
+    os.write((const char *)&len, sizeof len);
+
+    // Szöveg karaktereinek kiírása
+    os.write(this->c_str(), len);
+}
+
+
+//! ---------- BEOLVASÁS ----------
+
+// CÉL: A sztring visszaolvasása bináris streamből
+void PString::read(std::istream& is) {
+    std::size_t len = 0;
+
+    // Hossz beolvasása binárisan
+    is.read((char *)&len, sizeof len);
+
+    // Ide olvassuk be a szöveg karaktereit
+    std::string buf(len, '\0');
+
+    // Pontosan len darab karakter beolvasása
+    is.read(&buf[0], len);
+
+    // A beolvasott szövegből új PString lesz
+    *this = PString(buf.c_str());
+}
+
+
+//! ---------- DESTRUKTOR ----------
+
+// CÉL: Virtuális destruktor megvalósítása
+PString::~PString() {}
--- lab10/PString-master/pstring.h
+++ lab10/PString-master/pstring.h
@@ -1,65 +1,45 @@
-/**
- * \file pstring.h
- *
- * Szerializálható String osztály (PString = Persistent String)
- * A PString osztály a String osztályból és a Serializable interfészből örökli a tagfüggvényeit.
- */
-
 #ifndef PSTRING_H
 #define PSTRING_H
 
 #include "string5.h"
 #include "serializable.h"
+#include <iosfwd>
 
-/**
- * PString osztály.
- * A String osztályból és a Serializable interfészből örökli a tagfüggvényeit.
- * Képes az adatait szerializálni (write) és deszerializálni (read).
- */
+//! ---------- PSTRING OSZTÁLY ----------
+
+// CÉL: Olyan String, ami fileba írható és filebol visszaolvasható
 class PString : public String, public Serializable {
 public:
-    /// Konstruktor egy nullával lezárt char sorozatból (default és C-sztring)
-    /// @param p - pointer egy C sztringre (alapértéke: "")
-    PString(const char *p = "") : String(p) {}
 
-    /// Konstruktor egy karakterből
-    /// @param ch - karakter
-    PString(char ch) : String(ch) {}
+    //! ---------- KONSTRUKTOROK ----------
 
-    /// Másoló konstruktor String-ből
-    /// @param s - String, amiből létrehozzuk az új PString-et
-    PString(const String& s) : String(s) {}
+    PString(const char *p = "");
 
-    /// Másoló konstruktor PString-ből
-    /// @param ps - PString, amiből létrehozzuk az új PString-et
-    PString(const PString& ps) : String(ps) {}
+    PString(char ch);
 
-    /// write metódus: a sztringet szerializálja az adatfolyamra
-    /// Először a hosszt írja ki (size_t, bináris formában),
-    /// majd az adatokat (karakterek, beleértve az \n-t is).
-    /// @param os - kimeneti adatfolyam
-    virtual void write(std::ostream& os) const {
-        size_t len = this->size();
-        os.write(reinterpret_cast<const char*>(&len), sizeof(size_t));
-        os.write(this->c_str(), len);
-    }
+    PString(const String& s);
 
-    /// read metódus: a sztringet deszerializálja az adatfolyamról
-    /// Először a hosszt olvassa be (size_t, bináris formában),
-    /// majd az adatokat (karakterek, beleértve az \n-t is).
-    /// @param is - bemeneti adatfolyam
-    virtual void read(std::istream& is) {
-        size_t len;
-        is.read(reinterpret_cast<char*>(&len), sizeof(size_t));
-        char *buffer = new char[len + 1];
-        is.read(buffer, len);
-        buffer[len] = '\0';
-        *this = PString(buffer);
-        delete[] buffer;
-    }
+    PString(const PString& ps);
 
-    /// Destruktor
-    virtual ~PString() {}
+
+    //! ---------- ÍRÁS ----------
+
+    // CÉL: A PString tartalmának kiírása bin streambe
+    // Először a hossz, utána a szöveg
+    virtual void write(std::ostream& os) const;
+
+
+    //! ---------- OLVASÁS ----------
+
+    // CÉL: A PString tartalmának kiírása bin streambe
+    // Először a hossz, utána a szöveg
+    virtual void read(std::istream& is);
+
+
+    //! ---------- DESTRUKTOR ----------
+
+    // CÉL: Leszármaztatott osztály miatt virtuális destruktor
+    virtual ~PString();
 };
 
 #endif // PSTRING_H
