e7d20a63ae3b1b882554d8fbb95801231a3efdf3
PeterRapcsak <peter.rapcsak@gmail.com>
2026-05-19 23:48:30 +0200

    xd
    xd

--- lab11/ClonableHetero-master/clonable_bacterium.h
+++ lab11/ClonableHetero-master/clonable_bacterium.h
@@ -9,8 +9,6 @@
 #include "bacterium.h"
 #include "clonable.h"
 
-#error "itt készítse el Baktérium osztályok klonozható változatát"
-
 /**
  * A ClonableBacterium, ClonableSalmonella, ClonableStreptococcus
  * osztályoknak kompatibilisnek kell lennie a Bacterium és Clonable osztályokkal.
@@ -18,6 +16,46 @@
  * osztályok funkcióival.
  */
 
+
+//! ---------- KLÓNOZHATÓ BACTERIUM ----------
+struct ClonableBacterium : virtual Bacterium, Clonable {
+
+    ClonableBacterium(const char* n = "")
+        : Bacterium(n)
+    {}
+
+    ClonableBacterium* clone() const {
+        return new ClonableBacterium(*this);
+    }
+};
+
+
+//! ---------- KLÓNOZHATÓ SALMONELLA ----------
+struct ClonableSalmonella : Salmonella, ClonableBacterium {
+
+    ClonableSalmonella(const char* sp = "")
+        : Bacterium("Salmonella"),
+          Salmonella(sp)
+    {}
+
+    ClonableSalmonella* clone() const {
+        return new ClonableSalmonella(*this);
+    }
+};
+
+
+//! ---------- KLÓNOZHATÓ STREPTOCOCCUS ----------
+struct ClonableStreptococcus : Streptococcus, ClonableBacterium {
+
+    ClonableStreptococcus(char grp = 'A')
+        : Bacterium("Streptococcus"),
+          Streptococcus(grp)
+    {}
+
+    ClonableStreptococcus* clone() const {
+        return new ClonableStreptococcus(*this);
+    }
+};
 
 
 #endif // CLONABLE_BACTERIUM_H
--- lab11/ClonableHetero-master/clonable_hetero_store.hpp
+++ lab11/ClonableHetero-master/clonable_hetero_store.hpp
@@ -1,4 +1,4 @@
-﻿/**
+﻿ /**
  * \file: clonable_hetero_store.hpp
  *
  */
@@ -6,6 +6,126 @@
 #ifndef CLONABLE_HETERO_STORE_HPP
 #define CLONABLE_HETERO_STORE_HPP
 
-#error "itt készítse el a ClonableHeteroStore sablont"
+#include <vector>
+#include <stdexcept>
+
+//! ---------- ALAPÉRTELMEZETT KIVÉTEL ----------
+struct DefaultException : std::out_of_range {
+    DefaultException(const std::string& msg = "")
+        : std::out_of_range(msg)
+    {}
+};
+
+
+//! ---------- ALAPÉRTELMEZETT TÁROLÓ ----------
+template <typename T>
+struct DefaultContainer : std::vector<T> {};
+
+
+//! ---------- KLÓNOZHATÓ HETEROGÉN TÁROLÓ ----------
+template <
+    typename Base,                            // Alaposztály
+    typename Container = std::vector<Base*>,  // Belső tároló
+    typename Exception = std::out_of_range    // Throw kivétel
+>
+class ClonableHeteroStore {
+private:
+    Container store;
+
+public:
+    typedef typename Container::const_iterator const_iterator;
+
+
+    //! ---------- KONSTRUKTOROK / DESTRUKTOR ----------
+    ClonableHeteroStore()
+        : store()
+    {}
+
+    ~ClonableHeteroStore() {
+        clear();
+    }
+
+    ClonableHeteroStore(const ClonableHeteroStore& other)
+        : store()
+    {
+        for (const_iterator it = other.begin(); it != other.end(); ++it) {
+
+            if (*it != NULL)
+                add((*it)->clone());
+
+            else {
+                try {
+                    store.push_back(NULL);
+                } catch (std::bad_alloc&) {
+                    throw Exception("Memoria allokacios hiba a copy konstruktorban");
+                }
+            }
+        }
+    }
+
+
+    //! ---------- ÉRTÉKADÁS ----------
+    ClonableHeteroStore& operator=(const ClonableHeteroStore& other) {
+
+        if (this != &other) {
+            clear();
+
+            for (const_iterator it = other.begin(); it != other.end(); ++it) {
+
+                if (*it != NULL)
+                    add((*it)->clone());
+                else {
+                    try {
+                        store.push_back(NULL);
+                    } catch (std::bad_alloc&) {
+                        throw Exception("Memoria allokacios hiba az ertekadasban");
+                    }
+                }
+            }
+        }
+
+        return *this;
+    }
+
+
+    //! ---------- ITERÁTOROK ----------
+    const_iterator begin() const {
+        return store.begin();
+    }
+
+    const_iterator end() const {
+        return store.end();
+    }
+
+    std::size_t size() const {
+        return store.size();
+    }
+
+
+    //! ---------- ADATOK KEZELÉSE ----------
+    void add(Base* p) {
+
+        try {
+            store.push_back(p);
+        } catch (std::bad_alloc&) {
+
+            if (p != NULL)
+                delete p;
+
+            throw Exception("Nem sikerult az objektumot a taroloba betenni");
+        }
+    }
+
+    void clear() {
+
+        for (const_iterator it = store.begin(); it != store.end(); ++it) {
+
+            if (*it != NULL)
+                delete *it;
+        }
+
+        store.clear();
+    }
+};
 
 #endif // CLONABLE_HETERO_STORE_HPP
