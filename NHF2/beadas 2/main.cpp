8b9d619eb62f05b00b3b9f281ddfedee964f0262
PeterRapcsak <peter.rapcsak@gmail.com>
2026-05-24 19:40:11 +0200

    Final

---
+++ NHF2/beadas 2/main.cpp
@@ -0,0 +1,38 @@
+/*======================================================================
+    main.cpp - Főprogram
+----------------------------------------------------------------------
+    FELADAT:
+     - Game objektum létrehozása
+     - Kérdések betöltése CSV fileokból
+     - Főmenü (loop) indítása
+
+    //! Fordítási parancs:
+    //! g++ -std=c++11 -Wall -Wextra -DMEMTRACE -o loim main.cpp game.cpp highscore.cpp question.cpp filemanager.cpp test.cpp memtrace.cpp utils.cpp
+======================================================================*/
+
+#include "memtrace.h" // memóriaszivárgás check
+
+#include "game.h"
+
+// teszteléshez, később törölhető
+int tesztelek(bool run);
+
+// CÉL: CSV filenevek
+const char* FILE_CHOOSE = "kerdesek.csv";    // feleletválasztós kérdések
+const char* FILE_ORDER  = "sorkerdesek.csv"; // sorrendezős kérdések
+
+// CÉL: Program belépési pont
+int main() {
+    
+    // TESZTELŐ FÜGGVÉNY, ha nem akarjuk futtatni akkor csak simán írjuk át tesztelek(false)-ra
+    // test.cpp file-t futtatja
+    //! gtest_lite használtaával
+    tesztelek(true);
+
+
+    Game game; // konstruktor betölti a dicsőséglistát
+    game.loadQuestions(FILE_CHOOSE, FILE_ORDER); // CSV fileok betöltése kérdéstömbökbe
+    game.mainMenu(); // főmenü ciklus indítása
+
+    return 0;
+}
